f798a82491caca092f44cc4ce248f7278b94c9f4
Ahmed <ahmedchouihi@gmail.com>
2021-06-05 17:19:31 +0100

    Initial commit

---
+++ app/Student.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'paid',
+        'user_id',
+        'parent_id',
+        'class_id',
+        'roll_number',
+        'evaluation',
+        'gender',
+        'phone',
+        'dateofbirth',
+        'current_address',
+        'permanent_address',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Parents::class);
+    }
+
+    public function class()
+    {
+        return $this->belongsTo(Grade::class, 'class_id');
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+}
---
+++ resources/views/backend/students/contact.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="roles">
+
+        <div class="flex items-center justify-between mb-6">
+            <div>
+                <h2 class="text-gray-700 uppercase font-bold">Contact Student</h2>
+            </div>
+            <div class="flex flex-wrap items-center">
+                <a href="{{ route('student.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
+                    <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
+                    <span class="ml-2 text-xs font-semibold">Back</span>
+                </a>
+            </div>
+
+        </div>
+
+        <h2>Contact Form</h2>
+
+
+    </div>
+    <div class="container">
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <form method="POST" action="/contact">
+            @csrf
+            <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                <label for="email">Email address</label>
+                <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                       placeholder="Enter your email">
+                <span class="text-danger">{{ $errors->first('email') }}</span>
+            </div>
+            <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                <label for="name">Name</label>
+                <input name="name" type="text" class="form-control" id="name" aria-describedby="name" placeholder="Your name">
+                <span class="text-danger">{{ $errors->first('name') }}</span>
+
+            </div>
+            <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                <label for="exampleInputPassword1">Comment</label>
+                <textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <span class="text-danger">{{ $errors->first('comment') }}</span>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        $(function() {
+            $( "#datepicker-se" ).datepicker({ dateFormat: 'yy-mm-dd' });
+        })
+    </script>
+@endpush
